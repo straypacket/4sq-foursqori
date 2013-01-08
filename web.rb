7f8ef389c9f212b26ba67c24a8ecf2ca9c846844
Daniel Pereira <d.pereira@skillupjapan.co.jp>
2013-01-09 08:42:30 +0900

    Got rid of /test

--- web.rb
+++ web.rb
@@ -36,16 +36,3 @@ end
 get '/success' do
 	"Congrats, you just linked your account to the amazing new app!"
 end
-
-get '/test' do
-	cli_id = "A1YEYC2T2MBCVEJU51HPNKZA4XLL41WQ24WMRCI0FAA5BCHS"
-	cli_sec = "41T5JDOD3U5DGYIUFJVWCDF1CC1NI1A3WSH51EPJHGW5E04V"
-	red_uri = "http://badger.herokuapp.com/test"
-
-	# Make request with params[:code]
-	req = "https://foursquare.com/oauth2/access_token?client_id=#{cli_id}&client_secret=#{cli_sec}&grant_type=authorization_code&redirect_uri=#{red_uri}&code=#{params[:code]}"
-  	rep = open(req).read
-  	rep_j = JSON.parse(rep)
-  	access_token = rep_j['access_token']
-  	redirect '/success'
-end
