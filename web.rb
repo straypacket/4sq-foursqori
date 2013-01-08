e015912a82ef7a99c28d067423ce32831b807bb6
Daniel Pereira <d.pereira@skillupjapan.co.jp>
2013-01-08 17:00:05 +0900

    Back to original redirect

--- web.rb
+++ web.rb
@@ -31,13 +31,12 @@ end
 get '/test' do
 	cli_id = "A1YEYC2T2MBCVEJU51HPNKZA4XLL41WQ24WMRCI0FAA5BCHS"
 	cli_sec = "41T5JDOD3U5DGYIUFJVWCDF1CC1NI1A3WSH51EPJHGW5E04V"
-	red_uri = "http://badger.herokuapp.com/success"
+	red_uri = "http://badger.herokuapp.com/test"
 
 	# Make request with params[:code]
 	req = "https://foursquare.com/oauth2/access_token?client_id=#{cli_id}&client_secret=#{cli_sec}&grant_type=authorization_code&redirect_uri=#{red_uri}&code=#{params[:code]}"
   	rep = open(req).read
   	rep_j = JSON.parse(rep)
   	access_token = rep_j['access_token']
-  	access_token
-  	#redirect '/success'
+  	redirect '/success'
 end
