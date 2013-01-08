e84a72efbd6dea68f379847039718f760359f8ae
Daniel Pereira <d.pereira@skillupjapan.co.jp>
2013-01-08 16:22:03 +0900

    Initial redirect for registration

--- web.rb
+++ web.rb
@@ -21,5 +21,11 @@ get '/push' do
 end
 
 get '/test' do
-	params
+	cli_id = "A1YEYC2T2MBCVEJU51HPNKZA4XLL41WQ24WMRCI0FAA5BCHS"
+	cli_sec = "41T5JDOD3U5DGYIUFJVWCDF1CC1NI1A3WSH51EPJHGW5E04V"
+	red_uri = "http://badger.herokuapp.com/test"
+
+	# Make request params[:code]
+	#https://foursquare.com/oauth2/access_token?client_id=cli_id&client_secret=cli_sec&grant_type=authorization_code&redirect_uri=red_uri&code=params[:code]
+	params[:code]
 end
