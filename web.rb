522f770232a1ffe5fb2bfff27a28117cbabec72c
Daniel Pereira <d.pereira@skillupjapan.co.jp>
2013-01-08 16:54:58 +0900

    Added redirect to congratulate user

--- web.rb
+++ web.rb
@@ -2,6 +2,8 @@ require 'sinatra'
 require 'open-uri'
 require 'json'
 
+access_token = ''
+
 get '/' do
 	"Nothing to see, move along"
 end
@@ -22,6 +24,10 @@ get '/push' do
 	"Pushing ..."
 end
 
+get '/success' do
+	"Congrats, you just linked your account to the amazing new app!"
+end
+
 get '/test' do
 	cli_id = "A1YEYC2T2MBCVEJU51HPNKZA4XLL41WQ24WMRCI0FAA5BCHS"
 	cli_sec = "41T5JDOD3U5DGYIUFJVWCDF1CC1NI1A3WSH51EPJHGW5E04V"
@@ -31,5 +37,6 @@ get '/test' do
 	req = "https://foursquare.com/oauth2/access_token?client_id=#{cli_id}&client_secret=#{cli_sec}&grant_type=authorization_code&redirect_uri=#{red_uri}&code=#{params[:code]}"
   	rep = open(req).read
   	rep_j = JSON.parse(rep)
-  	rep_j['access_token']
+  	access_token = rep_j['access_token']
+  	redirect '/success'
 end
