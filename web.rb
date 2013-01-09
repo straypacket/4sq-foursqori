c8ed19839c94298c94d0147dc957e70480f15b14
Daniel Pereira <d.pereira@skillupjapan.co.jp>
2013-01-09 17:35:21 +0900

    Added oUATH and Version in request

--- web.rb
+++ web.rb
@@ -5,7 +5,7 @@ require 'net/http'
 require 'uri'
 use Rack::Logger
 
-access_token = ''
+access_token = 'JXQU5GQ5P3J1XCPUC1P1IIKMN1YFBFYKOPAT423FOA35DMKE'
 user = {}
 
 helpers do
@@ -37,18 +37,16 @@ get '/privacy' do
 end
 
 post '/push' do
-	#checkinID = "50ed174be4b0ca0b1eee4c4d"
-	checkinID = JSON.parse(params['checkin'])['id']
-	uri = URI.parse("https://api.foursquare.com/v2/checkins/#{checkinID}/addpost?oauth_token=ACCESS_TOKEN")
-	msg = {"text" => "Awesomeness!", "url" => "http://badger.herokuapp.com/", "contentId" => "my_ID"}
+	checkinID = "50ed1b29e4b07aef2815178e"
+	#checkinID = JSON.parse(params['checkin'])['id']
+	uri = URI.parse("https://api.foursquare.com/v2/checkins/#{checkinID}/addpost?oauth_token=#{access_token}&v=20130108")
 
 	logger.info params
-	logger.info "https://api.foursquare.com/v2/checkins/#{checkinID}/addpost"
-	logger.info msg
+	logger.info "https://api.foursquare.com/v2/checkins/#{checkinID}/addpost?oauth_token=#{access_token}&v=20130108"
 
 	http = Net::HTTP.new(uri.host, uri.port)
 	request = Net::HTTP::Post.new(uri.request_uri)
-	request.set_form_data(msg)
+	request.set_form_data({"text" => "Awesomeness", "url" => "http://badger.herokuapp.com/", "contentId" => "my_ID"})
 	request["Content-Type"] = "application/json"
 	response = http.request(request)
 
