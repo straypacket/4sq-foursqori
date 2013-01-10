9d9486147d73467b60ad243b354698426a2e6b09
Daniel Pereira <d.pereira@skillupjapan.co.jp>
2013-01-10 15:01:54 +0900

    Get user info

--- web.rb
+++ web.rb
@@ -29,17 +29,21 @@ get '/callback' do
 	logger.info user_code
 
 
-	# Make request with params[:code]
-	#req = "https://foursquare.com/oauth2/access_token?client_id=#{cli_id}&client_secret=#{cli_sec}&grant_type=authorization_code&redirect_uri=#{redir_uri}&code=#{user_code}"
-  	req = "https://foursquare.com/oauth2/authenticate?client_id=#{cli_id}&response_type=token&redirect_uri=#{redir_uri}"
+	# Get access token
+	req = "https://foursquare.com/oauth2/access_token?client_id=#{cli_id}&client_secret=#{cli_sec}&grant_type=authorization_code&redirect_uri=#{redir_uri}&code=#{user_code}"
   	rep = open(req).read
   	logger.info rep
-  	rep
-  	#rep_j = JSON.parse(rep)
-  	#access_token = rep_j['access_token']
+  	rep_j = JSON.parse(rep)
+  	access_token = rep_j['access_token']
+  	logger.info access_token
+
+  	# Get user info
+	req = "https://api.foursquare.com/v2/users/self?oauth_token=#{access_token}"
+  	rep = open(req).read
+  	logger.info rep
+
   	#user[user_code] = access_token
-  	#logger.info access_token
-  	#redirect '/success'
+  	redirect '/success'
 end
 
 get '/privacy' do
