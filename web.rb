55917306aa49b48fc34be527917d7ce33a0d8c55
Daniel Pereira <d.pereira@skillupjapan.co.jp>
2013-01-10 14:47:34 +0900

    Callback debug with new url

--- web.rb
+++ web.rb
@@ -30,14 +30,15 @@ get '/callback' do
 
 
 	# Make request with params[:code]
-	req = "https://foursquare.com/oauth2/access_token?client_id=#{cli_id}&client_secret=#{cli_sec}&grant_type=authorization_code&redirect_uri=#{redir_uri}&code=#{params[:code]}"
+	#req = "https://foursquare.com/oauth2/access_token?client_id=#{cli_id}&client_secret=#{cli_sec}&grant_type=authorization_code&redirect_uri=#{redir_uri}&code=#{user_code}"
+  	req = "https://foursquare.com/oauth2/authenticate?client_id=#{cli_id}&response_type=token&redirect_uri=#{redir_uri}"
   	rep = open(req).read
   	logger.info rep
-  	rep_j = JSON.parse(rep)
-  	access_token = rep_j['access_token']
-  	user[user_code] = access_token
-  	logger.info access_token
-  	redirect '/success'
+  	#rep_j = JSON.parse(rep)
+  	#access_token = rep_j['access_token']
+  	#user[user_code] = access_token
+  	#logger.info access_token
+  	#redirect '/success'
 end
 
 get '/privacy' do
