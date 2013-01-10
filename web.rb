473fd38c32477235f7e31b0fb6a7f737519ebd51
Daniel Pereira <d.pereira@skillupjapan.co.jp>
2013-01-10 14:22:03 +0900

    Callback debug

--- web.rb
+++ web.rb
@@ -20,23 +20,30 @@ get '/' do
 	"Nothing to see, move along"
 end
 
+get '/test' do
+	logger.info params
+	"Testing ..."
+end
+
 get '/callback' do
 	cli_id = "TIIWASIOG5LKB11BSVAMHTYBDVLUQDHTTJJHY4WTFBLU3EUQ"
 	cli_sec = "3EW5M1APICBDW1HMHH4LUYH25KTDP4ZWOM3R4TPE1NFFIBRU"
-	red_uri = "http://badger.herokuapp.com/callback"
+	redir_uri = "http://badger.herokuapp.com/test"
+	logger.info params
 	user_code = params['code']
 	logger.info user_code
 
 
 	# Make request with params[:code]
-	req = "https://foursquare.com/oauth2/access_token?client_id=#{cli_id}&client_secret=#{cli_sec}&grant_type=authorization_code&redirect_uri=#{red_uri}&code=#{params[:code]}"
+	#req = "https://foursquare.com/oauth2/access_token?client_id=#{cli_id}&client_secret=#{cli_sec}&grant_type=authorization_code&redirect_uri=#{redir_uri}&code=#{params[:code]}"
+  	req = "https://foursquare.com/oauth2/authenticate?client_id=#{cli_id}&response_type=token&redirect_uri=#{redir_uri}"
   	rep = open(req).read
   	logger.info rep
   	rep_j = JSON.parse(rep)
   	access_token = rep_j['access_token']
   	user[user_code] = access_token
   	logger.info access_token
-  	redirect '/success'
+  	#redirect '/success'
 end
 
 get '/privacy' do
@@ -64,5 +71,6 @@ post '/push' do
 end
 
 get '/success' do
+	logger.info params
 	"Congrats, you just linked your account to the amazing Qori app!"
 end
