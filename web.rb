be4bdba25db6877e8c7a759106172571e78de172
Daniel Pereira <d.pereira@skillupjapan.co.jp>
2013-01-10 14:31:06 +0900

    Callback debug

--- web.rb
+++ web.rb
@@ -21,28 +21,28 @@ get '/' do
 end
 
 get '/test' do
-	logger.info params
+	logger.info '/test '+params
 	"Testing ..."
 end
 
 get '/callback' do
 	cli_id = "TIIWASIOG5LKB11BSVAMHTYBDVLUQDHTTJJHY4WTFBLU3EUQ"
 	cli_sec = "3EW5M1APICBDW1HMHH4LUYH25KTDP4ZWOM3R4TPE1NFFIBRU"
-	redir_uri = "http://badger.herokuapp.com/test"
-	logger.info params
+	redir_uri = "http://badger.herokuapp.com/callback"
+	logger.info '/callback '+params
 	user_code = params['code']
-	logger.info user_code
+	logger.info '/callback '+user_code
 
 
 	# Make request with params[:code]
 	req = "https://foursquare.com/oauth2/access_token?client_id=#{cli_id}&client_secret=#{cli_sec}&grant_type=authorization_code&redirect_uri=#{redir_uri}&code=#{params[:code]}"
   	#req = "https://foursquare.com/oauth2/authenticate?client_id=#{cli_id}&response_type=token&redirect_uri=#{redir_uri}"
   	rep = open(req).read
-  	logger.info rep
+  	logger.info '/callback '+rep
   	rep_j = JSON.parse(rep)
   	access_token = rep_j['access_token']
   	user[user_code] = access_token
-  	logger.info access_token
+  	logger.info '/callback '+access_token
   	#redirect '/success'
 end
 
@@ -59,7 +59,7 @@ post '/push' do
 	uri = URI.parse(url)
 	msg = {"text" => "Advertisement", "url" => "http://badger.herokuapp.com/", "contentId" => "my_ID"}
 
-	logger.info url
+	logger.info '/push '+url
 
 	http = Net::HTTP.new(uri.host, uri.port)
 	http.use_ssl = true
@@ -67,10 +67,10 @@ post '/push' do
 	request = Net::HTTP::Post.new(uri.request_uri)
 	request.set_form_data(msg)
 	response = http.request(request)
-	logger.info response
+	logger.info '/push '+response
 end
 
 get '/success' do
-	logger.info params
+	logger.info '/success '+params
 	"Congrats, you just linked your account to the amazing Qori app!"
 end
