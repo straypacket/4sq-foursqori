dcdbeed20e9d6489c8819ded1ecab2f414d178f1
Daniel Pereira <d.pereira@skillupjapan.co.jp>
2013-01-10 12:19:08 +0900

    More debug

--- web.rb
+++ web.rb
@@ -27,6 +27,7 @@ get '/callback' do
 	# Make request with params[:code]
 	req = "https://foursquare.com/oauth2/access_token?client_id=#{cli_id}&client_secret=#{cli_sec}&grant_type=authorization_code&redirect_uri=#{red_uri}&code=#{params[:code]}"
   	rep = open(req).read
+  	logger.info rep
   	rep_j = JSON.parse(rep)
   	access_token = rep_j['access_token']
   	logger.info access_token
@@ -58,5 +59,5 @@ post '/push' do
 end
 
 get '/success' do
-	"Congrats, you just linked your account to the amazing new app!"
+	"Congrats, you just linked your account to the amazing Qori app!"
 end
