b7b84c4c54b9617d58b46b1b855e78a383284df4
Daniel Pereira <d.pereira@skillupjapan.co.jp>
2013-01-11 10:10:50 +0900

    Correctly get lat and lon

--- web.rb
+++ web.rb
@@ -109,7 +109,7 @@ post '/push' do
 
 	#Get geofence
 	location = JSON.parse(params['checkin'])['venue']['location']
-	req = "http://geo.skillupjapan.net/squares/get_random_geo_object?long=#{location.lat}&lat=#{location.lon}"
+	req = "http://geo.skillupjapan.net/squares/get_random_geo_object?long=#{location['lat']}&lat=#{location['lon']}"
 	logger.info req
   	rep = open(req).read
   	rep_j = JSON.parse(rep)
